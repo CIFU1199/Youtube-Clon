0332e1f8255ca936ca420d543cb51d4704612c21
CIFU1199 <edwkamilo@gmail.com>
2022-11-28 17:02:28 -0500

    NavBar Finish

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    
+    /* screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    }, */
+    colors: {
+      'bg-color':'#181818',
+      'dark-bg': '#212121',
+      'dark-800':'#27272a', 
+      'dark-900':'#18181b',
+      'red-logo' : '#ff0000',
+      'white' : '#ffffff'
+      
+    },
+    fontFamily: {
+      Roboto: ['Roboto', 'sans-serif'],
+      Montserrat: ['Montserrat', 'sans-serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
   },
   plugins: [],
 }
