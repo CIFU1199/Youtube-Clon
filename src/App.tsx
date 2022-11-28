f41dec94575eb25017f1fd531f331606e8481172
CIFU1199 <edwkamilo@gmail.com>
2022-11-28 12:13:34 -0500

    React Router Dom Setup Finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Search from "./pages/Search"
+import Watch from "./pages/Watch"
 
 const App = () => {
   return (
-    <h1 className='text-3xl font-bold underline'>Hola Mundo</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/search" element={<Search/>}/>
+        <Route path="/watch/:id" element={<Watch/>}/>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
